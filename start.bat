edb49371ee35e5a76737730bd6e9eac50f42bb13
YoshimasaFujisawa <yoshimasa.fujisawa@botti.yokohama>
2026-03-06 23:13:44 +0900

    feat: auto-open browser on startup for Windows and Mac

--- start.bat
+++ start.bat
@@ -1,5 +1,9 @@
 @echo off
 cd /d %~dp0
 echo STEN-F 起動準備中...
+
+:: バックグラウンドで約6秒待機してからブラウザを開く
+start "" cmd /c "ping localhost -n 7 >nul & start http://localhost:3000"
+
 uv run reflex run
 pause
--- start.command
+++ start.command
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 echo "STEN-F 起動準備中..."
+
+# バックグラウンドで6秒待機してからブラウザを開く
+(sleep 6 && open "http://localhost:3000") &
+
 uv run reflex run
